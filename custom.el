a45aa0d668b2d65ad7fed1d4308b49e1d156ce3a
Michael Chan <mijoch@gmail.com>
2015-08-30 12:02:02 -0700

    emacs(custom): remove scroll and tool bar

--- custom.el
+++ custom.el
@@ -6,7 +6,9 @@
  '(magit-log-arguments (quote ("--graph" "--color" "--decorate")))
  '(package-selected-packages
    (quote
-    (coffee-mode auto-complete helm-ag evil-surround key-chord web-mode org-plus-contrib markdown-mode magit jsx-mode js2-mode helm-projectile flycheck evil base16-theme))))
+    (sass-mode helm-ag helm-projectile coffee-mode auto-complete evil-surround key-chord web-mode org-plus-contrib markdown-mode magit jsx-mode js2-mode flycheck evil base16-theme)))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
